272a93c3c0553a1871f0aaae116a03d350480df5
Josue Guevara <josueguevara291@gmail.com>
2021-03-12 13:51:21 -0500

    Manipulación de Strings

---
+++ String manipulation.py
@@ -0,0 +1,15 @@
+frase = "Datahack, donde estudiar con datos es sencillo y edificante"
+
+cap = frase.capitalize()
+low = frase.lower()
+up = frase.upper()
+swap = frase.swapcase()
+tit = frase.title()
+
+print(f"Frase original: {frase}")
+print(f"Frase capitalizada: {cap}")
+print(f"Frase en minúsculas: {low}")
+print(f"Frase en mayúsculas: {up}")
+print(f"Frase cambiada: {swap}")
+print(f"Frase titulada: {tit}")
+
